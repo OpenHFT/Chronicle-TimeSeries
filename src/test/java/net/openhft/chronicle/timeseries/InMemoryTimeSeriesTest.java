b05aae2fa57644d3b4dec11c356c82f10f35a7e4
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-03-01 17:29:24 +0000

    Add a partial writeSome method and a compact method.

--- src/test/java/net/openhft/chronicle/timeseries/InMemoryTimeSeriesTest.java
+++ src/test/java/net/openhft/chronicle/timeseries/InMemoryTimeSeriesTest.java
@@ -100,43 +100,9 @@ public class InMemoryTimeSeriesTest {
         mid2.generateBrownian(1, 2, 0.0005);
 
         // compare the correlation
-//    CorralationStatistic stats = PearsonsCorrelation.calcCorrelation(mid, mid2, Mode.AFTER_BOTH_CHANGE);
+//    CorrelationStatistic stats = PearsonsCorrelation.calcCorrelation(mid, mid2, Mode.AFTER_BOTH_CHANGE);
     }
 
-    @Test
-    @Ignore("TODO FIX")
-    public void testGenerateRandomSequence() {
-        long size = 600_000_000L;
-
-        // generate series 1
-        TimeSeries ts = new InMemoryTimeSeries(null);
-        ts.setLength(size);
-
-        LongColumn time = ts.getTimestamp();
-        time.setAll(Random::new, (c, i, r) -> c.set(i, 9 + (int) Math.pow(1e6, sqr(r.nextFloat()))));
-        long sum = time.integrate(); // sum all the intervals
-
-        System.out.printf("%.1f days%n", sum / 86400e6);
-
-        DoubleColumn mid = ts.acquireDoubleColumn("mid", BytesDoubleLookups.INT16_4);
-        mid.generateBrownian(1, 2, 0.0005);
-
-        // generate series 2
-        TimeSeries ts2 = new InMemoryTimeSeries(null);
-        ts2.setLength(size);
-
-        LongColumn time2 = ts.getTimestamp();
-        time2.setAll(Random::new, (c, i, r) -> c.set(i, 9 + (int) Math.pow(1e6, sqr(r.nextFloat()))));
-        long sum2 = time2.integrate(); // sum all the intervals
-
-        System.out.printf("%.1f days%n", sum2 / 86400e6);
-
-        DoubleColumn mid2 = ts2.acquireDoubleColumn("mid", BytesDoubleLookups.INT16_4);
-        mid2.generateBrownian(1, 2, 0.0005);
-
-        // compare the correlation
-//    CorralationStatistic stats = PearsonsCorrelation.calcCorrelation(mid, mid2, Mode.AFTER_BOTH_CHANGE);
-    }
 
     static float sqr(float f) {
         return f * f;
